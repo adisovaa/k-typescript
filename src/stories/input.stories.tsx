9bba4a867407fef89f0c9a9440fac10ad08caa90
adisovaa <79251934+adisovaa@users.noreply.github.com>
2024-03-01 20:07:22 -0600

    '13 controlled input, checkbox, select'

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -8,16 +8,12 @@ export default {
 export const UncontrolledInput = () => <input/>
 export const TrackValueOfUncontrolledInputWith = () => {
     const [value, setValue] = useState('')
-
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        return setValue(e.currentTarget.value)
-    }
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
 
     return <>
         <input onChange={onChange}/> - {value} -
     </>
 }
-
 export const GetValueOfUncontrolledInputWith = () => {
     const [value, setValue] = useState('')
     const inputRef = useRef<HTMLInputElement>(null)
@@ -33,7 +29,35 @@ export const GetValueOfUncontrolledInputWith = () => {
         - {value} -
     </>
 }
-
-
 export const ControlledInputWithFixedValue = () => <input value={'Middle developer'}/>
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
 
+    return <>
+        <input value={parentValue} onChange={onChange}/>
+        <div>{parentValue}</div>
+    </>
+}
+export const ControlledCheckbox = () => {
+    const [checked, setChecked] = useState<boolean>(false)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked)
+    return <>
+        <input type={"checkbox"} checked={checked} onChange={onChange}/>
+    </>
+}
+export const ControlledSelect = () => {
+    const [selected, setSelected] = useState<string | undefined>('2')
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => setSelected(e.currentTarget.value)
+
+    return <>
+        <select value={selected} onChange={onChange}>
+            <option value={'1'}>1</option>
+            <option value={'2'}>2</option>
+            <option value={'3'}>3</option>
+            <option value={'4'}>4</option>
+            <option value={'5'}>5</option>
+        </select>
+        --- {selected} ---
+    </>
+}
